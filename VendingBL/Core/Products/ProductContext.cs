3bbbb6ba6110183f805c07d7b39fb971bcc608a8
sandman633 <kshigapov-1999@mai.ru>
2020-09-15 17:27:45 +0300

    Удалил тест(лул). Добавил энтити(Зачем?)

---
+++ VendingBL/Core/Products/ProductContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingBL.Core.Products
+{
+    class ProductContext : DbContext
+    {
+        public ProductContext(): base("DbConnection")
+        { }
+        public DbSet<Snack> Snacks { get; set; }
+
+    }
+}
