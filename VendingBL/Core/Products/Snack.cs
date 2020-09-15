2551ef4b74298554185940999e00c1f01874bbff
sandman633 <kshigapov-1999@mai.ru>
2020-09-15 17:35:47 +0300

    Revert "Удалил тест(лул). Добавил энтити(Зачем?)"
    This reverts commit 3bbbb6ba6110183f805c07d7b39fb971bcc608a8.

--- VendingBL/Core/Products/Snack.cs
+++ VendingBL/Core/Products/Snack.cs
@@ -8,7 +8,6 @@ namespace VendingBL.Core.Products
 {
     public class Snack
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Count { get; set; }
